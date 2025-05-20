9fad62e6be80fd78aabdaab73e7880b67870b2f5
Sethi Dhiyal <sethidhial906@gmail.com>
2025-05-20 17:19:52 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -3,20 +3,13 @@ const app = express();
 const db = require("./db");
 require("dotenv").config();
 
-
-
 // Add middleware to parse JSON request bodies
 app.use(express.json());
 const PORT = process.env.PORT || 4000;
 
-
 app.get("/", function (req, res) {
   res.send("Welcome to the Goa, how can I help you?");
 });
-
-
-
-
 
 //import the router files
 const personRoutes = require("./routes/personRoutes");
@@ -25,8 +18,6 @@ const menuItemRoutes = require("./routes/menuItemRoutes");
 //use the routers
 app.use("/person", personRoutes);
 app.use("/menu", menuItemRoutes);
-
-
 
 app.listen(PORT, () => {
   console.log("Server is running on port 4000:---");
